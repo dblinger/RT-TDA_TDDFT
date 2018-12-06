5f05590a44571725b98a4e7f6469e63c48958f1c
Lingerfelt <6dl@mac102882.ornl.gov>
2018-12-06 10:02:38 -0500

    added comments for the print_coords.py script

--- print_coords.py
+++ print_coords.py
@@ -1,3 +1,9 @@
+#!/bin/python
+#
+#   Script to print the coordinates in the plane.  
+#   Note that the fastest running index should be made
+#   consistent with the ordering of the cube file format
+#
 from __future__ import division
 import sys
 
