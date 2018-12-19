8afebb936232259c00ff0cc90e328e31e27b99d3
Lingerfelt <6dl@mac102882.ornl.gov>
2018-12-19 12:40:31 -0500

    generalized the form_cube script to arbitrary number of z points

--- form_cube.py
+++ form_cube.py
@@ -1,15 +1,12 @@
 import numpy as np
 import sys
 
-numx = 21
-numy = 21
 numz = 6
 
-#note this only works because there are exactly 6 z points
 data = np.loadtxt(str(sys.argv[1]))
 coups = data[:,2]
 for i in range(coups.shape[0]):
   print coups[i],
-  if (i+1)%5==0: print
-
+  if (i+1)%6==0: print
+  if (i+1)%numz==0 and (i+1)%6!=0: print
 
