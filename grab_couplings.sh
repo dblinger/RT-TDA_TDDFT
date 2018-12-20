8866658fae19eb9cfae2f3d855240d1880705eae
Lingerfelt <6dl@mac102882.ornl.gov>
2018-12-20 12:46:53 -0500

    fixed the grep'd string in grab_couplings to be unique to the couplings

--- grab_couplings.sh
+++ grab_couplings.sh
@@ -1,6 +1,6 @@
 for i in `seq 1 5000`;
 do
-	grep "      $i  " couplings.log | grep "   0   " > $i.dat
+        grep "                  $i  " couplings.log | grep "   0   " > $i.dat
 	awk '{ total += $3 } END { print total/NR }' $i.dat >> avg_coups.dat
 done
 
