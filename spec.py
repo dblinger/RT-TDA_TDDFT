14427516400dcdbc7d938a00001f448ee5d5e08f
dblinger <dblinger@uw.edu>
2018-12-27 11:22:38 -0500

    Update spec.py

--- spec.py
+++ spec.py
@@ -4,26 +4,21 @@ import sys
 import numpy as np
 from scipy.fftpack import fft, fftfreq
 
-def spec(time,dip):
-    '''
-timedip input file should have time (in AU) in first column, then x, y, and z dipole components
-    '''
-   
-    # chronusq file is CSV, also skip the header (first row)
+#timedip input file should have time (in AU) in first column, then x, y, and z dipole components
 
+def spec(time,dip):
+   
     # subtract the average to prevent zero peak 
     dip = dip - np.mean(dip)
 
-    # do the fourier transform 
+    # do the discrete fourier transform 
     fw = fft(dip)
 
     # determine frequency range
-    n = len(fw)                         # number samples, including padding
-    timestep = time[1] - time[0]              # spacing between time samples; assumes constant time step
+    n = len(fw)      
+    timestep = time[1] - time[0]              # assumes constant time step
     w = fftfreq(n,d=timestep)*2.0*np.pi # frequency list
    
-    fw_re = np.real(fw)                 # the real FFT frequencies
-    fw_im = (np.imag(fw))               # the imaginary FFT frequencies
     fw_abs = abs(fw)                    # absolute value of frequencies
     
     w = (w*27.2114)    # give frequencies in eV
