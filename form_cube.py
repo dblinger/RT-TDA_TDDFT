78951820fa30b4fe469c4d6efc3012d786a87b1f
dblinger <dblinger@uw.edu>
2019-01-08 14:55:51 -0500

    Update form_cube.py

--- form_cube.py
+++ form_cube.py
@@ -1,21 +1,12 @@
-import numpy as np
 import sys
 
-numz = 81
+numz = 21
 
-#account for the index starting at zero...
 data = np.loadtxt(str(sys.argv[1]))
 coups = data[:,2]
-count=1
-for i in range(1,coups.shape[0]+1):
-  print coups[i-1],
-  if (count%6==0):
-    count=1
-    print
-    continue
-  else:
-    count += 1
-  if (i%numz==0):
-    print
-    count=1
+for i in range(coups.shape[0]):
+  print coups[i],
+  if (i+1)%6==0: print
+  if (i+1)%numz==0 and (i+1)%6!=0: print
+
 
